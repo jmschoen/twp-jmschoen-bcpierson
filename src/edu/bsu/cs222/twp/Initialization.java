8e4177fbc11482c5083fb04e2b40afcf79eab78c
Brian Pierson <14piersonb@gmail.com>
2015-09-16 17:48:27 -0400

    Created Revision and Initialization class
    contains basic username field and functionality

---
+++ src/edu/bsu/cs222/twp/Initialization.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs222.twp;
+
+public class Initialization {
+	public static void main(String[] args){
+		Revision r = new Revision();
+		System.out.println(r.getUsername());
+	}
+}
---
+++ src/edu/bsu/cs222/twp/Revision.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222.twp;
+
+public class Revision {
+	private String username;
+	
+	public Revision(){
+		username = "you";
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+}
