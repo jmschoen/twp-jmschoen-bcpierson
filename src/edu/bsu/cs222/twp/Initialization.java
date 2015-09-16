4ea63757feab256801d08091f6da7a2c20f663bf
Brian Pierson <14piersonb@gmail.com>
2015-09-16 19:08:15 -0400

    got UI working

--- src/edu/bsu/cs222/twp/Initialization.java
+++ src/edu/bsu/cs222/twp/Initialization.java
@@ -4,5 +4,19 @@ public class Initialization {
 	public static void main(String[] args){
 		Revision r = new Revision();
 		System.out.println(r.getUsername());
+		
+		
+		new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(UI.class);
+            }
+        }.start();
+        UI userInterface = UI.waitForStartUpTest();
+       //.outputData();
+        
+       // UI userInterface = new UI();
+		//UI.launch(args);
+		userInterface.outputData(r.getUsername());
 	}
 }
