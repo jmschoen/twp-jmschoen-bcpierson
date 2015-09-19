1de540db2e0dde80bd6227d60ee49c22c6f0b77d
Brian Pierson <14piersonb@gmail.com>
2015-09-19 17:18:05 -0400

    added data to Revision objects and created a toString for Revisions

--- src/edu/bsu/cs222/twp/Initialization.java
+++ src/edu/bsu/cs222/twp/Initialization.java
@@ -2,9 +2,7 @@ package edu.bsu.cs222.twp;
 
 public class Initialization {
 	public static void main(String[] args){
-		Revision r = new Revision();
 		XMLParser x = new XMLParser();
-		System.out.println(r.getUsername());
 		new Thread() {
             @Override
             public void run() {
--- src/edu/bsu/cs222/twp/Revision.java
+++ src/edu/bsu/cs222/twp/Revision.java
@@ -1,13 +1,27 @@
 package edu.bsu.cs222.twp;
 
 public class Revision {
-	private String username;
 	
-	public Revision(){
-		username = "you";
+	private String username;
+	private String date;
+	private String time;
+	//private String comment;
+	
+	public Revision(String inputUsername, String inputDate, String inputTime){
+		username = inputUsername;
+		date = inputDate;
+		time = inputTime;
 	}
 	
 	public String getUsername(){
 		return username;
+	}
+	
+	@Override
+	public String toString(){
+		return "Name: " + username +
+				"\n\tDate: " + date +
+				"\n\tTime: " + time + "\n";
+		
 	}
 }
